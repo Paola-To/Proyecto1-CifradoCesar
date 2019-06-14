ed568865780816d81447d0782284442a16cd5de0
Paola Torres <paola.t.betancourt@gmail.com>
2019-06-13 19:12:06 -0500

    Subiendo Proyecto 1 Cifrado Cesar

---
+++ src/cipher1.js
@@ -0,0 +1,7 @@
+document.getElementById("start").addEventListener("click", ()=>{
+  location.href = "index.html"
+})
+
+document.getElementById("exit").addEventListener("click", ()=>{
+  location.href = "exit.html"
+})
---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.getElementById("cipher").addEventListener("click", ()=>{
+  location.href = "cipher.html"
+})
+
+document.getElementById("decipher").addEventListener("click", ()=>{
+  location.href = "decipher.html"
+})
