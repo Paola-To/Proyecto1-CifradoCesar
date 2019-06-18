0d0874d79d7b4cabbba816ae210a971a5611121e
Paola Torres <paola.t.betancourt@gmail.com>
2019-06-18 18:40:06 -0500

    Agregue función de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,15 @@
 window.cipher = {
- encode () => {}
+ encode: (offset,string) => {
+   let result = '';
+   string = string.toUpperCase();
+   for (let i = 0; i<string.length;i++){
+    const ascii = string[i].charCodeAt();
+    const formula = (ascii-65+offset)%26+65;
+    const letter = String.fromCharCode(formula)
+    result = result + letter
+   }
+   return result
+
+ },
+ decode: (offset,string) => {}
 };
--- src/cipher1.js
+++ src/cipher1.js
@@ -5,3 +5,19 @@ document.getElementById("start").addEventListener("click", ()=>{
 document.getElementById("exit").addEventListener("click", ()=>{
   location.href = "exit.html"
 })
+
+
+let txtCipher = document.getElementById("txt-cipher")
+
+let offsetCipher = document.getElementById ("offset-cipher")
+
+let cipherButton = document.getElementById ("cipher-button")
+
+let messageCipher = document.getElementById ("message-cipher")
+
+cipherButton.addEventListener("click", ()=>{
+ let string = txtCipher.value.toUpperCase()
+ let offset = offsetCipher.value
+ let cifrado = window.cipher.encode(offset,string)
+ messageCipher.value = cifrado
+})
